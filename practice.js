6062f51acb4161b03c5a17d9eb542f2f986df8db
abdullhsharker <abdullahsharker99@gmail.com>
2022-02-22 22:55:10 +0600

    first commit

---
+++ practice.js
@@ -0,0 +1,28 @@
+// number 1 problem
+// const favFood = ['phitha', 'peyara', 'badam', 'chicken-fry', 'modhu', 'pizza', 'mashroom', 'mango'];
+// const search = ['m'];
+// const outPut = [];
+
+// for (const food of favFood) {
+//     if (food.indexOf(search) != -1) {
+//         outPut.push(food);
+//     }
+// }
+// console.log(outPut);
+
+// number 2 problem
+
+// const friendsName = ['md rubel', 'jubayer', 'Md munna', 'tipu', 'Md risal', 'bishal', 'md sanik'];
+// const search = 'md';
+// const nobabFriend = [];
+// for (const name of friendsName) {
+//     if (name.toLocaleLowerCase().indexOf(search.toLocaleLowerCase()) != -1) {
+//         nobabFriend.push(name);
+//     }
+// }
+// console.log(nobabFriend);
+
+// number 3 problem
+const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+const specialFruits = fruits.slice(1, 3);
+console.log(specialFruits);
